4947de2cdc9868c4ed64ce11231d37ccf3730e41
Mengqi Yu <mengqiy@google.com>
2021-03-31 22:26:34 -0700

    init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mengqiy/kpt-openapi-proto-poc
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/googleapis/gnostic v0.5.4
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+	openapi_v2 "github.com/googleapis/gnostic/openapiv2"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	jb, err := ioutil.ReadFile("./openapi.json")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("length of bytes for input json openapi doc: %v\n", len(jb))
+
+	doc, err := openapi_v2.ParseDocument(jb)
+	if err != nil {
+		return err
+	}
+
+	pb, err := proto.Marshal(doc)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("length of bytes for the proto serialized openapi doc: %v\n", len(pb))
+
+	var newDoc openapi_v2.Document
+	if err = proto.Unmarshal(pb, &newDoc); err != nil {
+		return err
+	}
+
+	swg := newDoc.String()
+	fmt.Printf("length of string for the json serialized openapi doc: %v\n", len(swg))
+	fmt.Printf("Printing part of the serialized json openapi doc:\n %s\n", swg[:200])
+
+	return nil
+}
